a15948eb9b0afbbe84c4c972474c408e2b3220f5
Anderson Rodrigues Dias <anderson33rd@hotmail.com>
2022-09-04 21:40:57 -0300

    Dimdim

---
+++ WebDIMDIM/Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebDIMDIM.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto ( DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
---
+++ WebDIMDIM/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebDIMDIM.Models
+{
+    [Table("tb_produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código de Identificação")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Column("Valor")]
+        [Display(Name = "Valor do Produto")]
+        public double Valor { get; set; }
+
+        [Column("Quantidade")]
+        [Display(Name = "Quantidade")]
+        public int Quantidade { get; set; }
+    }
+}
